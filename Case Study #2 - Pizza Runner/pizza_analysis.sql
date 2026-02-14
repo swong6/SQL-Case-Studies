ed1329cd9d62dcbb012605fd10b13800d332fa0d
Sharmaine Wong <99834601+swong6@users.noreply.github.com>
2026-02-14 13:49:59 -0800

    Add SQL analysis for pizza orders and runners
    Created temporary tables for customer and runner orders, handling nulls and data formatting. Added initial pizza metrics queries for analysis.

---
+++ Case Study #2 - Pizza Runner/pizza_analysis.sql
@@ -0,0 +1,83 @@
+-- (1) Temp Table - customer_orders 
+-- handling nulls w/ case when
+DROP TABLE IF EXISTS customer_orders_temp;
+
+CREATE TEMPORARY TABLE customer_orders_temp AS
+SELECT order_id,
+       customer_id,
+       pizza_id,
+       CASE
+           WHEN exclusions = '' THEN NULL
+           WHEN exclusions = 'null' THEN NULL
+           ELSE exclusions
+       END AS exclusions,
+       CASE
+           WHEN extras = '' THEN NULL
+           WHEN extras = 'null' THEN NULL
+           ELSE extras
+       END AS extras,
+       order_time
+FROM customer_orders;
+
+SELECT * FROM customer_orders_temp;
+ 
+ -- (2) Temp Table - runner_orders 
+-- nulls (pickup_time, distance, duration, cancellation): replaced with ' '
+-- distance: replace 'km' with ' '
+-- duration: replace 'minutes', 'min' with ' '
+DROP TABLE IF EXISTS runner_orders_temp;
+
+CREATE TEMPORARY TABLE runner_orders_temp AS
+
+SELECT order_id,
+       runner_id,
+       CASE
+           WHEN pickup_time LIKE 'null' THEN NULL
+           ELSE pickup_time
+       END AS pickup_time,
+       CASE
+           WHEN distance LIKE 'null' THEN NULL
+           ELSE CAST(regexp_replace(distance, '[a-z]+', '') AS FLOAT)
+       END AS distance,
+       CASE
+           WHEN duration LIKE 'null' THEN NULL
+           ELSE CAST(regexp_replace(duration, '[a-z]+', '') AS FLOAT)
+       END AS duration,
+       CASE
+           WHEN cancellation LIKE '' THEN NULL
+           WHEN cancellation LIKE 'null' THEN NULL
+           ELSE cancellation
+       END AS cancellation
+FROM runner_orders;
+
+SELECT * FROM runner_orders_temp;
+
+-- A. Pizza Metrics
+-- How many pizzas were ordered?
+select	count(*) as pizza_count
+from customer_orders_temp
+;
+
+-- How many unique customer orders were made?
+select	count(distinct order_id) as unique_order_count
+from customer_orders_temp
+;
+-- How many successful orders were delivered by each runner?
+
+-- output -- runner_id | successful_count
+-- successful_count = where cancellation is null
+
+select	runner_id,
+		count(*) as successful_count
+from runner_orders_temp 
+where cancellation = ' '
+group by runner_id
+;
+
+-- How many of each type of pizza was delivered?
+-- How many Vegetarian and Meatlovers were ordered by each customer?
+-- What was the maximum number of pizzas delivered in a single order?
+-- For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+-- How many pizzas were delivered that had both exclusions and extras?
+-- What was the total volume of pizzas ordered for each hour of the day?
+-- What was the volume of orders for each day of the week?
