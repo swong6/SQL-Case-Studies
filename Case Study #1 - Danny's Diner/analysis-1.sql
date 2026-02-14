95b73ea12190033392b66edc553a0d94221ea3c2
Sharmaine Wong <99834601+swong6@users.noreply.github.com>
2026-02-14 02:23:15 -0800

    Fix visit count and points calculation in analysis-1.sql
    Updated SQL queries to improve accuracy in visit counts and points calculation.

--- Case Study #1 - Danny's Diner/analysis-1.sql
+++ Case Study #1 - Danny's Diner/analysis-1.sql
@@ -14,7 +14,7 @@ order by customer_id
 
 -- 2. How many days has each customer visited the restaurant?
 select	customer_id, 
-		count(order_date) as visit_count
+		count(distinct order_date) as visit_count
 from sales
 group by customer_id
 order by customer_id
@@ -23,7 +23,7 @@ order by customer_id
 -- 3. What was the first item from the menu purchased by each customer?
 with cte as (
 select *,
-		row_number() over (partition by customer_id order by order_date asc) as ranking
+		rank() over (partition by customer_id order by order_date asc) as ranking
 from sales s 
 inner join menu m 
 on s.product_id = m.product_id
@@ -144,4 +144,59 @@ order by customer_id
 ;
 
 -- 9.  If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
+
+-- points earned = price*10
+-- sushi points = price*10 where product_name = 'sushi'
+
+-- (1) case when s.product_id = 1 then price*20 
+--		else price*20
+--		end as points
+
+-- output -- customer_id, points
+with cte as (
+select s.customer_id,
+  		s.product_id,
+  		m1.price,
+		(case
+        	when m1.product_name = 'sushi' then price*20 
+        else price*10
+		end) as points
+from sales s 
+inner join menu m1
+on s.product_id = m1.product_id
+inner join members m2
+on m2.customer_id = s.customer_id
+)
+select customer_id, 
+		sum(points) as total_points
+from cte
+group by customer_id
+;
+
 -- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
+
+with cte as (
+select s.customer_id,
+(case when 
+		s.order_date >= m2.join_date and 
+ 		s.order_date < join_date + interval '7 days'
+		then price*20
+ 	when
+ 		m1.product_name = 'sushi' then price*20
+	else price*10
+ 	end) as points
+from sales s 
+inner join menu m1
+on s.product_id = m1.product_id
+inner join members m2
+on m2.customer_id = s.customer_id
+where order_date >= '2021-01-01'
+  	and order_date <= '2021-01-31'
+
+)
+
+select customer_id,
+		sum(points) as total_points
+from cte
+group by customer_id
+;
