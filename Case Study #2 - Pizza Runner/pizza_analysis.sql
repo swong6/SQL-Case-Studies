48d63f3e9c7b0b09e46d61581de10d0c80b5b21b
Sharmaine Wong <99834601+swong6@users.noreply.github.com>
2026-02-15 01:11:34 -0800

    Add SQL queries for pizza delivery analysis

--- Case Study #2 - Pizza Runner/pizza_analysis.sql
+++ Case Study #2 - Pizza Runner/pizza_analysis.sql
@@ -105,7 +105,76 @@ order by c.customer_id
 ;
 
 -- What was the maximum number of pizzas delivered in a single order?
+-- output -- c.order_id, max_pizza_ct
+
+select	c.order_id,	
+		count(c.pizza_id) as max_pizza_ct
+from customer_orders_temp c 
+inner join runner_orders_temp r 
+where cancellation is null
+group by c.order_id
+order by c.order_id, max_pizza_ct desc
+limit 1 
+;
+
 -- For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+
+-- delivered = r.cancellation is null
+-- 1_change = exclusion or extras is not null
+-- no_change = exclusion and extras is null
+
+-- output -- c.customer_id | count(1_change) | count(no_change)
+
+select	c.customer_id,
+		sum(case
+        	when c.exclusions is not null
+            	or c.extras is not null
+            then 1 else 0 end) as one_change,
+		sum(case
+        	when c.exclusions is null 
+            	and c.extras is null
+            then 1 else 0 end) as no_change
+from customer_orders_temp c 
+inner join runner_orders_temp r 
+on c.order_id = r.order_id
+where r.cancellation is null
+group by c.customer_id
+order by c.customer_id
+;
+
 -- How many pizzas were delivered that had both exclusions and extras?
+
+-- output | count(BOTH exclusions and extras is not null
+
+select	c.customer_id,	
+		sum(case
+        	when c.exclusions is not null
+            	and c.extras is not null
+            then 1 else 0 end) as both_changes
+from customer_orders_temp c 
+inner join runner_orders_temp r 
+on c.order_id = r.order_id
+where r.cancellation is null
+group by c.customer_id
+;
 -- What was the total volume of pizzas ordered for each hour of the day?
+
+-- group by hour
+-- total_volume = count(*)
+
+select	hour(order_time) as hour_of_day,	
+		count(*) as pizza_ct,
+        round(100*count(*)/sum(count(*)) over(),2) as volume
+from customer_orders_temp c 
+group by hour_of_day
+order by hour_of_day
+;
+
 -- What was the volume of orders for each day of the week?
+
+select	dayname(order_time) as day_of_week,	
+		count(*) as pizza_ct,
+        round(100*count(*)/sum(count(*)) over(),2) as volume
+from customer_orders_temp c 
+group by day_of_week
+order by day_of_week desc
